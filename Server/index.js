80093272b386d52877e172b68e0e10b82d7f965b
chetanchaudhari07 <chetancdi077@gmail.com>
2025-07-11 11:43:20 +0530

    updated

--- Server/index.js
+++ Server/index.js
@@ -6,11 +6,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-pp.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
-app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 
 const Todo = require("./routes/todoRoutes");
